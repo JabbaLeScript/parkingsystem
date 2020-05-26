de7895f30d5cd1ab0fb3d2530f90b68c4dbdb2ed
JabbaLeScript <alexandre.vieir3@gmail.com>
2020-05-26 18:23:10 +0200

    FareCalculatorService.java: - Creation d'une methode formatter pour transformer le resultat en minute ou en heure

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -6,10 +6,16 @@ import com.parkit.parkingsystem.model.Ticket;
 import java.sql.Time;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class FareCalculatorService {
 
+    private double formatter(long aDuration, Function<Long, Double> aTransformation){
+        return aTransformation.apply(aDuration);
+    }
+
     public void calculateFare(Ticket ticket){
+
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
@@ -20,12 +26,8 @@ public class FareCalculatorService {
         //TODO: Some tests are failing here. Need to check if this logic is correct
         long duration = outHour - inHour;
 
-
-        double durationHour = TimeUnit.MILLISECONDS.toHours(duration);
-
-        long durationMinute = TimeUnit.MILLISECONDS.toMinutes(duration);
-
-        double percentRate = (double) durationMinute / (double) 60;
+        double durationHour = formatter(duration, (aLong) -> Double.valueOf(TimeUnit.MILLISECONDS.toHours(duration)));
+        double durationMinute = formatter(duration, (aLong) -> Double.valueOf(TimeUnit.MILLISECONDS.toMinutes(duration)));
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
@@ -37,7 +39,7 @@ public class FareCalculatorService {
                         ticket.setPrice(0.0);
                     }
                     else {
-                    ticket.setPrice(percentRate * Fare.CAR_RATE_PER_HOUR);
+                    ticket.setPrice(durationMinute / 60 * Fare.CAR_RATE_PER_HOUR);
                     }
                 }
                 break;
@@ -47,7 +49,7 @@ public class FareCalculatorService {
                 ticket.setPrice(durationHour * Fare.BIKE_RATE_PER_HOUR);
                 }
                 else {
-                    ticket.setPrice(percentRate * Fare.BIKE_RATE_PER_HOUR);
+                    ticket.setPrice(durationMinute / 60 * Fare.BIKE_RATE_PER_HOUR);
                 }
                 break;
             }
--- src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/FareCalculatorServiceTest.java
@@ -19,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.Date;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class FareCalculatorServiceTest {
